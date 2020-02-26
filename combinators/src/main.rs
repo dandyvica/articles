8192be50795e0511c634643f78379a97e778034f
dandyvica <dandyvica@gmail.com>
2020-02-26 22:26:41 +0100

    First commit for articles backup

--- combinators/src/main.rs
+++ combinators/src/main.rs
@@ -12,15 +12,22 @@ fn main() -> Result<(), Box<dyn Error>> {
     // easy
     //----------------------------------------------------------------------
 
-    // count()
+    // count(): get the number of natural elements
     let n = v.iter().count();
     assert_eq!(n, 118);
 
-    // last()
+    // last(): which is the last element's name ?
     let last_element = v.iter().last().unwrap();
     assert_eq!(last_element.element, "Oganesson");
 
-    // map
+    // nth(): Uranium is the 92th element (vector is indexing from 0)
+    let uranium = v.iter().nth(91).unwrap();
+    assert_eq!(uranium.element, "Uranium");
+    assert!(v.iter().nth(300).is_none());
+
+    // map(): create a new vector with Element's type fields
+    let mut mapped = v.iter().map(|x| (x.atomic_number, &x.element, &x.symbol));
+    //assert_eq!(mapped.nth(5).unwrap(), (1, "Carbon".to_string(), "C".to_string()));
 
     // take()
     let first_2: Vec<_> = v.iter().take(2).map(|x| x.element.clone()).collect();
@@ -30,26 +37,22 @@ fn main() -> Result<(), Box<dyn Error>> {
     let less_than_10e = v.iter().take_while(|x| x.number_of_neutrons <= 10);
     assert_eq!(less_than_10e.count(), 10);
 
-    // nth()
-    let uranium = v.iter().nth(91).unwrap();
-    assert_eq!(uranium.element, "Uranium");
-
-
     // any()
+    assert!(v.iter().any(|x| x.number_of_neutrons > 50));
 
-    // all()
-
-    // take()
-
+    // all(): all elements are their symbol composed by 1 or 2 letters (e.g.: C or Na)
+    assert!(v.iter().all(|x| x.symbol.len() == 1 || x.symbol.len() == 2));
 
     // by_ref
+    //let by_ref = v.iter().by_ref().
 
     // chain
+    //let chain = v.iter().chain()
 
     // cycle
     assert_eq!(v.iter().cycle().nth(120).unwrap().element, "Lithium");
 
-    // find()
+    // find(): Helium is the first element whose name ends with "ium"
     let helium = v.iter().find(|x| x.element.ends_with("ium")).unwrap();
     assert_eq!(helium.element, "Helium");
 
@@ -70,6 +73,40 @@ fn main() -> Result<(), Box<dyn Error>> {
     assert_eq!(radioactives.iter().count(), 37);
 
     // flat_map()
+
+    // fuse()
+    //let fuse = v.iter()
+
+    // position
+    let potassium = v.iter().position(|x| x.element == "Potassium").unwrap();
+    assert_eq!(v[potassium].symbol, "K");
+
+    // rposition()
+    let last_gas = v.iter().rposition(|x| x.phase == "gas").unwrap();
+    assert_eq!(v[last_gas].element, "Radon");
+
+    // max_by(): which is the heaviest non-artificial element ? Uranium !
+    use std::cmp::Ordering;
+    let cmp = |x: &Element, y: &Element| -> Ordering {
+        if x.atomic_mass < y.atomic_mass {
+            Ordering::Less
+        } else if x.atomic_mass > y.atomic_mass {
+            Ordering::Greater
+        } else {
+            Ordering::Equal
+        }
+    };
+
+    let heaviest = v.iter().filter(|x| x.phase != "artificial").max_by(|x, y| cmp(x,y)).unwrap();
+    assert_eq!(heaviest.symbol, "U");
+
+    // last(): obvious !
+    let hydrogen = v.iter().rev().last().unwrap();
+    assert_eq!(hydrogen.symbol, "H");
+
+    // max_by_key(): longuest element name is Rutherfordium
+    let longuest = v.iter().max_by_key(|x| x.element.len()).unwrap();
+    assert_eq!(longuest.element, "Rutherfordium");
 
     Ok(())
 }
